b48eb92dd09aba7b446f25c6706d655289970738
aishwarya <aishwaryaj1608@gmail.com>
2025-09-06 17:02:55 +0530

    seeding check on atlas

---
+++ database/database.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+// Use Mongoose to connect your Node.js application to a MongoDB database
+export async function connectDatabase() {
+
+    try {
+
+        //connect to the MongoDB database 
+        // await mongoose.connect("mongodb://localhost:27017/ytcaish");
+        await mongoose.connect("mongodb+srv://aishwaryaj1608_db_user:c5LO2prcJAFTOFsZ@yt.lfypvmp.mongodb.net/");
+
+
+        //log confirmation message if successful
+        console.log(`Database connected successfully `);
+
+    } catch (err) {
+
+        //If connection fails, log the error message
+        console.log("Error while connecting to the database", err.message);
+        
+        // Exit the process with failure
+        process.exit(1);
+    }
+
+    //Log for disconnection events
+    mongoose.connection.on("disconnected" , ()=>{
+        console.log("MongoDB disconnected ")
+    })
+
+}
+
+export default connectDatabase;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
+import connectDatabase from './database/database.js';
+import {runAllSeeders} from "./seeder/masterSeeder.js"
 
 const app = express();
 app.use(cors());
@@ -11,16 +13,20 @@ app.listen(PORT,()=>{
     console.log(`server connected to port : ${PORT}`)
 });
 
-mongoose.connect('mongodb+srv://aishwaryaj1608_db_user:c5LO2prcJAFTOFsZ@yt.lfypvmp.mongodb.net/').then(()=>{
-    console.log("DB connected...")
-}).catch((err)=>{
-    console.log(err , "error while connecting...")
-});
+// mongoose.connect('mongodb+srv://aishwaryaj1608_db_user:c5LO2prcJAFTOFsZ@yt.lfypvmp.mongodb.net/').then(()=>{
+//     console.log("DB connected...")
+// }).catch((err)=>{
+//     console.log(err , "error while connecting...")
+// });
 
-app.get('/' , (req,res)=>{
-    res.send("Welcome to root route")
+// app.get('/' , (req,res)=>{
+//     res.send("Welcome to root route")
+// })
+
+connectDatabase().then(async ()=>{
+    await runAllSeeders();
+    app.listen(PORT , ()=> console.log(`Server running on port : ${PORT}`))
 })
-
 
 
 
